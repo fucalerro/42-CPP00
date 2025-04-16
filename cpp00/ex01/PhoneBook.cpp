451dc618cf496f14c141aa9f0ae890205f7df3e3
Luca Ferro <lferro@c1r17s6.42lausanne.ch>
2025-04-16 15:50:59 +0200

    iomanips

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <iomanip>
+
 
 PhoneBook::PhoneBook() {
   this->contact_count = 0;
@@ -16,16 +18,14 @@ std::string crop_string(const std::string value) {
   if (value.length() > 10) {
     return value.substr(0, 9) + '.';
   }
-  return std::string(10 - value.length(), ' ') + value;
+  return value;
 }
 
 void PhoneBook::search() {
   std::string header[] = {"index", "first name", "last name", "nickname"};
 
   for (int i = 0; i < 4; i++) {
-    std::cout << crop_string(header[i]);
-    if (i < 3)
-      std::cout << "|";
+    std::cout << std::setw(10) << std::right << crop_string(header[i]) << "|";
   }
   std::cout << std::endl;
 
@@ -35,13 +35,10 @@ void PhoneBook::search() {
     std::string last_name = this->contact[i].get_last_name();
     std::string nickname = this->contact[i].get_nickname();
     std::string darkest_secret = this->contact[i].get_darkest_secret();
-    std::cout << "         " << index;
-    std::cout << "|";
-    std::cout << crop_string(first_name);
-    std::cout << "|";
-    std::cout << crop_string(last_name);
-    std::cout << "|";
-    std::cout << crop_string(nickname);
+    std::cout << std::setw(10) << std::right << index << "|";
+    std::cout << std::setw(10) << std::right << crop_string(first_name) << "|";
+    std::cout << std::setw(10) << std::right << crop_string(last_name) << "|";
+    std::cout << std::setw(10) << std::right << crop_string(nickname) << "|";
     std::cout << std::endl;
   }
 
@@ -67,20 +64,18 @@ void PhoneBook::search() {
 
   Contact found_contact = this->contact[index];
 
-  std::cout << "First name: " << found_contact.get_first_name() << std::endl;
-  std::cout << "Last name: " << found_contact.get_last_name() << std::endl;
-  std::cout << "Nickname: " << found_contact.get_nickname() << std::endl;
-  std::cout << "Phone number: " << found_contact.get_phone_number()
-            << std::endl;
-  std::cout << "Darkest secret: " << found_contact.get_darkest_secret()
-            << std::endl;
+  std::cout << std::setw(20) << std::left << "First name: " << found_contact.get_first_name() << std::endl;
+  std::cout << std::setw(20) << std::left << "Last name: " << found_contact.get_last_name() << std::endl;
+  std::cout << std::setw(20) << std::left << "Nickname: " << found_contact.get_nickname() << std::endl;
+  std::cout << std::setw(20) << std::left << "Phone number: " << found_contact.get_phone_number() << std::endl;
+  std::cout << std::setw(20) << std::left << "Darkest secret: " << found_contact.get_darkest_secret() << std::endl;
 
   return;
 }
 
 std::string prompt(std::string message) {
   while (1) {
-    std::cout << message;
+    std::cout << std::setw(20) << std::left << message;
     std::string input;
     getline(std::cin, input);
     if (std::cin.eof()) {
